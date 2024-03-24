b9e06cb31f07155b9304cb8c6c4dc76f7c552118
DemonJAZ <163294328+jaspreetsinghmahal@users.noreply.github.com>
2024-03-24 19:59:38 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -2,6 +2,6 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (req, res) => res.send('Hello martina Welcome to our Server!'))
+router.get('/', (req, res) => res.send('Hello martina Welcome to our test Server!'))
 
 module.exports = router
